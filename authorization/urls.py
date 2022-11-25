88307592e9788cf7b1a2efe182e7e5c43ad43ab6
Anna Shkolina <annashkolina01@gmail.com>
2022-11-25 12:57:14 +0300

    Edit logout from profile

--- authorization/urls.py
+++ authorization/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('login', views.login),
-    path('registration', views.registration_user)
+    path('registration', views.registration_user),
+    path('logout', views.user_logout),
 ]
