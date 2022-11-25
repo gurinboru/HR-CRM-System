eda922779b7f97d82055f94d4d64137d4ed8e0ac
Tereshchenko Vlad <ddiavall@yandex.ru>
2022-11-25 11:57:13 +0300

    Added forms for job & candidate

--- start/forms.py
+++ start/forms.py
@@ -1,18 +1,27 @@
 from django import forms
+from django.contrib.auth.models import User
+from .models import Job
 
-class LoginForm(forms.Form):
-    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control login_signin', 'id':"floatingInput", "placeholder":"Login"}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control password_signin', "id":"floatingPassword", "placeholder":"Password"}))
-    checkboxRemember = forms.BooleanField(widget=forms.CheckboxInput(),required=False)
+class AddCandidateForm(forms.Form):
+    name = forms.CharField(widget=forms.TextInput)
+    phone = forms.CharField(widget=forms.TextInput)
+    email = forms.EmailField(widget=forms.EmailInput)
+    sex = forms.CharField(widget=forms.TextInput)
+    photo = forms.FileField(widget=forms.FileInput)
+    birthdate = forms.DateField(widget=forms.DateInput)
+    cv = forms.FileField(widget=forms.FileInput)
+    class Meta:
+        model = User
+        fields = ('username', 'phone', 'email', 'sex', 'photo', 'birthdate', 'cv')
 
-class RegistrationForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput(
-        attrs={"class": "form-control reg_middle", "minlength": "6", "id": "validationCustom03",
-               "placeholder": "Password"}))
-    password2 = forms.CharField(widget=forms.PasswordInput(
-        attrs={"class": "form-control", "id": "validationCustom04", "placeholder": "Confirm password"}))
-    email = forms.EmailField(widget=forms.EmailInput(
-        attrs={"class": "form-control reg_middle", "id": "validationCustom02", "placeholder": "name@example.com"}))
-    username = forms.CharField(widget=forms.TextInput(
-        attrs={"class": "form-control", "pattern": "\D[^А-Яа-я ]{1,20}", "id": "validationCustom01",
-               "placeholder": "loginexample"}))
+class AddJobForm(forms.Form):
+    name = forms.CharField(widget=forms.TextInput)
+    salary = forms.IntegerField(widget=forms.NumberInput)
+    expirence = forms.IntegerField(widget=forms.NumberInput)
+    employment = forms.CharField(widget=forms.TextInput)
+    definition = forms.CharField(widget=forms.TextInput)
+    id_status = forms.CharField(widget=forms.TextInput)
+
+    class Meta:
+        model = Job
+        fields = ('name', 'salary', 'expirence', 'employment', 'definition', 'id_status', 'user')
