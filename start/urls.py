e8ec0a78fae9babb09e939d3f6797e49446068cd
Anna Shkolina <annashkolina01@gmail.com>
2022-11-26 03:25:03 +0300

    Updated design

--- start/urls.py
+++ start/urls.py
@@ -10,8 +10,8 @@ urlpatterns = [
     path('change_job/<int:pk>', views.changeJob),
     path('change_jobseek/<int:pk>', views.changeJobSeek),
     path('add_jobseek', views.addJobSeek),
-    path('jobs',views.getJob),
-    path('jobseek',views.getJobSeek),
+    path('jobs', views.getJob),
+    path('jobseek', views.getJobSeek),
     path('add_job', views.addJob),
     path('cerjobseek/<int:pk>', views.getCerJobSeek, name='cerjobseek'),
     path('cercandidate/<int:pk>', views.getCerCandidate, name='cercandidate'),
@@ -19,6 +19,6 @@ urlpatterns = [
     path('candidates', views.getCandidates),
     path('jobs', views.getJob),
     path('jobseek', views.getJobSeek),
-    path('cercandidate/get_cv/<int:pk>',views.get_cv),
-    path('parseHTML',views.parseHTML)
+    path('cercandidate/get_cv/<int:pk>', views.get_cv),
+    path('parseHTML', views.parseHTML)
 ]
